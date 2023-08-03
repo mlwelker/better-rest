7de7f8534d0506dbca936f740dd8aa2d892f3acc
mlwelker <nutria.watts.0g@icloud.com>
2023-08-03 18:44:59 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael Welker on 2023-08-03.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
